7350a55ce10988ac8c8661fe45f3ff04dbe14528
naeemz11 <62848187+naeemz11@users.noreply.github.com>
2020-11-15 21:00:00 -0500

    Update article.rb

--- lib/article.rb
+++ lib/article.rb
@@ -47,6 +47,8 @@ class Article
        
            #binding.pry
            Launchy.open("#{self.all[index].url}")  
+       elsif 
+           index == 1
        else 
            puts "Please select 1 or 2".red
            self.view_article_in_browser?
